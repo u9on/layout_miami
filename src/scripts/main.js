8fec57ffcda6c0f732f8907bfdd366fe9cb3f96d
Jenya Golovko <u9on1210@gmail.com>
2023-05-23 23:41:47 +0300

    make build

--- src/scripts/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--width-menu');
-  } else {
-    document.body.classList.remove('page__body--width-menu');
-  }
-});
